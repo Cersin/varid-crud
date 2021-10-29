a63ef2d1aa934bf5658a562847085eaf1febfe70
Szymon Wójcik <cersin96@gmail.com>
2021-10-29 11:52:41 +0200

    Add Vuetify Data Table

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,150 @@
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <Table :data="contacts"></Table>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Table from "@/components/Table";
 export default {
-  data: () => ({ drawer: null }),
+  components: {Table},
+  data() {
+    return {
+      drawer: null,
+      dialog: false,
+      contacts: []
+    }
+  },
+  created () {
+    this.initialize()
+  },
+  methods: {
+    initialize () {
+      this.contacts = [
+        {
+          "id": 547,
+          "name": "Mareczek",
+          "last_name": "Winiarski",
+          "phone_number": "3434242",
+          "email": "test0@mail.com",
+          "country": "country0",
+          "city": "city0",
+          "address": "address0",
+          "created_at": "2021-10-28T15:36:17.000000Z",
+          "updated_at": "2021-10-28T19:35:27.000000Z"
+        },
+        {
+          "id": 546,
+          "name": "Johny",
+          "last_name": "Bravo",
+          "phone_number": "123456789",
+          "email": "j@j.pl",
+          "country": "Portugalia",
+          "city": "Lizbona",
+          "address": "prtthha 10/4",
+          "created_at": "2021-10-28T14:57:46.000000Z",
+          "updated_at": "2021-10-28T14:57:46.000000Z"
+        },
+        {
+          "id": 545,
+          "name": "Piotr",
+          "last_name": "Winiarek",
+          "phone_number": "654321987",
+          "email": "pio@pio.pl",
+          "country": "Chorwacja",
+          "city": "Split",
+          "address": "demartica 56/3",
+          "created_at": "2021-10-28T14:45:27.000000Z",
+          "updated_at": "2021-10-28T14:45:27.000000Z"
+        },
+        {
+          "id": 542,
+          "name": "Test",
+          "last_name": "Test",
+          "phone_number": "982379486",
+          "email": "marekw1996@gmail.com",
+          "country": "Polska",
+          "city": "Szczecin",
+          "address": "Test",
+          "created_at": "2021-10-28T14:12:54.000000Z",
+          "updated_at": "2021-10-28T14:12:54.000000Z"
+        },
+        {
+          "id": 541,
+          "name": "Alina",
+          "last_name": "Kowalska",
+          "phone_number": "945.468.9436",
+          "email": "alina.kowalska19@gmail.com",
+          "country": "Poland",
+          "city": "Gdansk",
+          "address": "80-547 Gdańsk, Władysława IV 13B 58 737-66-30, 737-66-62",
+          "created_at": "2021-10-28T12:38:14.000000Z",
+          "updated_at": "2021-10-28T12:38:14.000000Z"
+        },
+        {
+          "id": 540,
+          "name": "Jan",
+          "last_name": "Kowalski",
+          "phone_number": "945.458.9236",
+          "email": "jan.kowalski23@gmail.com",
+          "country": "Poland",
+          "city": "Gdansk",
+          "address": "80-547 Gdańsk, Władysława IV 13B 58 737-66-30, 737-66-62",
+          "created_at": "2021-10-28T12:37:03.000000Z",
+          "updated_at": "2021-10-28T12:37:03.000000Z"
+        },
+        {
+          "id": 538,
+          "name": "Krystel",
+          "last_name": "Osinski",
+          "phone_number": "940.438.9136",
+          "email": "lemke.reese@gmail.com",
+          "country": "Poland",
+          "city": "Warsaw",
+          "address": "2757 Rippin Gardens Apt. 010Aufderharfurt, TX 81917",
+          "created_at": "2021-10-28T07:41:22.000000Z",
+          "updated_at": "2021-10-28T07:41:22.000000Z"
+        },
+        {
+          "id": 537,
+          "name": "Krystel",
+          "last_name": "Osinski",
+          "phone_number": "940.438.9136",
+          "email": "lemke.reese@gmail.com",
+          "country": "Poland",
+          "city": "Warsaw",
+          "address": "2757 Rippin Gardens Apt. 010Aufderharfurt, TX 81917",
+          "created_at": "2021-10-28T07:37:22.000000Z",
+          "updated_at": "2021-10-28T07:37:22.000000Z"
+        },
+        {
+          "id": 536,
+          "name": "Krystel",
+          "last_name": "Osinski",
+          "phone_number": "940.438.9136",
+          "email": "lemke.reese@gmail.com",
+          "country": "Poland",
+          "city": "Warsaw",
+          "address": "2757 Rippin Gardens Apt. 010Aufderharfurt, TX 81917",
+          "created_at": "2021-10-27T15:05:15.000000Z",
+          "updated_at": "2021-10-27T15:05:15.000000Z"
+        },
+        {
+          "id": 535,
+          "name": "Krystel",
+          "last_name": "Osinski",
+          "phone_number": "940.438.9136",
+          "email": "lemke.reese@gmail.com",
+          "country": "Poland",
+          "city": "Warsaw",
+          "address": "2757 Rippin Gardens Apt. 010Aufderharfurt, TX 81917",
+          "created_at": "2021-10-27T15:05:14.000000Z",
+          "updated_at": "2021-10-27T15:05:14.000000Z"
+        }
+      ]
+    }
+  }
 }
 </script>
---
+++ src/components/Table.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-data-table
+      :headers="headers"
+      :items="data"
+      class="elevation-1"
+      :items-per-page="perPage"
+      :server-items-length="total"
+      :options.sync="options"
+  >
+    <template v-slot:top>
+      <v-toolbar
+          flat
+      >
+        <v-toolbar-title>Varid CRUD</v-toolbar-title>
+        <v-divider
+            class="mx-4"
+            inset
+            vertical
+        ></v-divider>
+        <v-spacer></v-spacer>
+
+<!--        <v-dialog v-model="dialogDelete" max-width="500px">-->
+<!--          <v-card>-->
+<!--            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>-->
+<!--            <v-card-actions>-->
+<!--              <v-spacer></v-spacer>-->
+<!--              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>-->
+<!--              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>-->
+<!--              <v-spacer></v-spacer>-->
+<!--            </v-card-actions>-->
+<!--          </v-card>-->
+<!--        </v-dialog>-->
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+          small
+          class="mr-2"
+          @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+          small
+          @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn
+          color="primary"
+          @click="initialize"
+      >
+        Reset
+      </v-btn>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  props: ['data'],
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    perPage: 5,
+    total: 68,
+    options: {
+      page: 1,
+      itemsPerPage: 10
+    },
+    headers: [
+      {
+        text: 'Name',
+        align: 'start',
+        sortable: false,
+        value: 'name',
+      },
+      { text: 'Last Name', value: 'last_name' },
+      { text: 'Phone', value: 'phone_number' },
+      { text: 'Email', value: 'email' },
+      { text: 'Country', value: 'country' },
+      { text: 'City', value: 'city' },
+      { text: 'Adress', value: 'address' },
+      { text: 'Actions', value: 'actions', sortable: false }
+    ],
+
+    contacts: [],
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
